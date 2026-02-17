a7e48d1796348b7e574ac5735ca85b6cee0c0d7c
Rushil54 <rushil2k4@gmail.com>
2026-02-17 17:04:08 +0530

    final

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
-const API_URL = 'https://expense-tracker-io8g.onrender.com';
+const API_URL = 'https://expense-tracker-2-kd15.onrender.com';
 
 // 1. Generate a fresh key when the app loads
 let currentIdempotencyKey = uuidv4();
